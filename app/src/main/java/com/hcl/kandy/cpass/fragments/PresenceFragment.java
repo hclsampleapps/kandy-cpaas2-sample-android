30ad24745f4a326c2c275a5ba3d40eb034bbac67
Ashish Goel <ashishgo@hcl.com>
2019-03-06 05:44:23 +0530

    Chat module commit.

---
+++ app/src/main/java/com/hcl/kandy/cpass/fragments/PresenceFragment.java
@@ -0,0 +1,13 @@
+package com.hcl.kandy.cpass.fragments;
+
+/**
+ * Created by Ashish Goel on 2/4/2019.
+ */
+public class PresenceFragment extends BaseFragment {
+    public PresenceFragment() {
+    }
+
+    public static PresenceFragment newInstance() {
+        return new PresenceFragment();
+    }
+}
---
+++ app/src/main/java/com/hcl/kandy/cpass/remote/API.java
@@ -0,0 +1,9 @@
+package com.hcl.kandy.cpass.remote;
+
+/**
+ * Created by Ashish Goel on 2/1/2019.
+ */
+public interface API {
+//    String BASE_URL = "https://nvs-cpaas-oauth.kandy.io";
+    String LOGIN_URL = "cpaas/auth/v1/token";
+}
