dcd99f1575051174483b5d1a64d2a9056fb93a27
Ashish Goel <ashishgo@hcl.com>
2019-03-07 05:09:06 +0530

    SMS module commit.

--- app/src/main/java/com/hcl/kandy/cpass/fragments/BaseFragment.java
+++ app/src/main/java/com/hcl/kandy/cpass/fragments/BaseFragment.java
@@ -1,9 +1,14 @@
 package com.hcl.kandy.cpass.fragments;
 
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.widget.EditText;
 
 /**
  * Created by Ashish Goel on 2/4/2019.
  */
 public class BaseFragment extends Fragment {
+    public void showMessage(EditText mEt, String message) {
+        Snackbar.make(mEt, message, Snackbar.LENGTH_SHORT).show();
+    }
 }
