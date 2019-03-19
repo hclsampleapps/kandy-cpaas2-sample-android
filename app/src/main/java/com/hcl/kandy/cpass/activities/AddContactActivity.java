b112d0fad940399af591f236651643921a396c09
Ashish Goel <ashishgo@hcl.com>
2019-03-20 03:50:38 +0530

    Added validation in Addressbook.

--- app/src/main/java/com/hcl/kandy/cpass/activities/AddContactActivity.java
+++ app/src/main/java/com/hcl/kandy/cpass/activities/AddContactActivity.java
@@ -3,6 +3,8 @@ package com.hcl.kandy.cpass.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -85,10 +87,15 @@ public class AddContactActivity extends BaseActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_create_contact:
-                if (mUpdateContact)
-                    updateContact();
-                else
-                    addContact();
+                String prContact = mEtPrimaryContact.getText().toString();
+                if (TextUtils.isEmpty(prContact)) {
+                    showMessage(mEtPrimaryContact, "Please add primary contact");
+                } else {
+                    if (mUpdateContact)
+                        updateContact();
+                    else
+                        addContact();
+                }
                 break;
         }
     }
