4961ccc5b77cda636098c9972aa8ac8c64a41f2d
Ashish Goel <ashishgo@hcl.com>
2019-03-27 02:56:29 +0530

    Presence function added

---
+++ app/src/main/java/com/hcl/kandy/cpass/adapters/PresenceAdapter.java
@@ -0,0 +1,11 @@
+package com.hcl.kandy.cpass.adapters;
+
+import com.rbbn.cpaas.mobile.presence.api.PresenceList;
+
+import java.util.List;
+
+public class PresenceAdapter {
+    public void setPresenceLists(List<PresenceList> mPresenceLists) {
+
+    }
+}
