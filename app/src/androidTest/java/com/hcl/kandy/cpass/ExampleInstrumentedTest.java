02059af45b0676c6a0e7bd6ae5e906679987b71a
Ashish Goel <ashishgo@hcl.com>
2021-12-29 23:00:58 +0530

    addressbook issue resolved

--- app/src/androidTest/java/com/hcl/kandy/cpass/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hcl/kandy/cpass/ExampleInstrumentedTest.java
@@ -1,15 +1,18 @@
 package com.hcl.kandy.cpass;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
@@ -135,12 +138,12 @@ public class ExampleInstrumentedTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         onView(withId(R.id.participant_address))
-                .perform(typeText(participant_address), closeSoftKeyboard());
+                .perform(clearText(),typeText(participant_address), closeSoftKeyboard());
+
         onView(withId(R.id.start_call_button)).perform(click());
         try {
-            Thread.sleep(20000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -161,6 +164,11 @@ public class ExampleInstrumentedTest {
         }
         onView(withId(R.id.recycleView))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(withId(R.id.et_last_name))
                 .perform(typeText("abcd"), closeSoftKeyboard());
         onView(withId(R.id.button_create_contact)).perform(click());
@@ -169,14 +177,18 @@ public class ExampleInstrumentedTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        onView(isRoot()).perform(ViewActions.pressBack());
+
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
-        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_presence));
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_group_chat));
 
         try {
-            Thread.sleep(10000);
+            Thread.sleep(20000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        onView(withId(R.id.groupList))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
 
 }
