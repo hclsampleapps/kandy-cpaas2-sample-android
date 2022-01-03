47b8dfe13f6bc8c623c8d53e7af567576e578855
Ashish Goel <ashishgo@hcl.com>
2022-01-03 21:50:48 +0530

    seprate test cases

--- app/src/androidTest/java/com/hcl/kandy/cpass/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hcl/kandy/cpass/ExampleInstrumentedTest.java
@@ -11,7 +11,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import static org.hamcrest.Matchers.anything;
 
 import androidx.test.core.app.ActivityScenario;
@@ -19,12 +18,10 @@ import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.hcl.kandy.cpass.activities.LoginActivity;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -44,6 +41,330 @@ public class ExampleInstrumentedTest {
     private String destinationAddress = "abc@gmail.com";
     private String participant_address = "abc@gmail.com";
 
+    @Test
+    public void verifyLoginModule() {
+
+        ActivityScenario.launch(LoginActivity.class);
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(uName), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(pwd), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(client), closeSoftKeyboard());
+
+        onView(withId(R.id.button_login)).perform(click());
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void verifyChatMessage() {
+
+        ActivityScenario.launch(LoginActivity.class);
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(uName), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(pwd), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(client), closeSoftKeyboard());
+
+        onView(withId(R.id.button_login)).perform(click());
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.container)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.etDestainationAddress))
+                .perform(typeText(destination), closeSoftKeyboard());
+        onView(withId(R.id.btnFetchChat)).perform(click());
+
+        onView(withId(R.id.etMessage))
+                .perform(typeText("hi"), closeSoftKeyboard());
+        onView(withId(R.id.btnStartChat)).perform(click());
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void verifySMS() {
+
+        ActivityScenario.launch(LoginActivity.class);
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(uName), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(pwd), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(client), closeSoftKeyboard());
+
+        // Clicks a button to send the message to another
+        // activity through an explicit intent.
+        onView(withId(R.id.button_login)).perform(click());
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.container)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_sms));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("+12102424496")).perform(click());
+        onView(withId(R.id.etDestainationAddress))
+                .perform(typeText(destinationSMS), closeSoftKeyboard());
+        onView(withId(R.id.etMessage))
+                .perform(typeText("hello"), closeSoftKeyboard());
+        onView(withId(R.id.btnStartSMS)).perform(click());
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void verifyMultimedia() {
+
+        ActivityScenario.launch(LoginActivity.class);
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(uName), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(pwd), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(client), closeSoftKeyboard());
+
+        // Clicks a button to send the message to another
+        // activity through an explicit intent.
+        onView(withId(R.id.button_login)).perform(click());
+
+        // Verifies that the DisplayMessageActivity received an intent
+        // with the correct package name and message.
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.container)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_multimedia));
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.etDestainationAddress))
+                .perform(typeText(destinationAddress), closeSoftKeyboard());
+
+        onView(withId(R.id.btnFetchChat)).perform(click());
+        onView(withId(R.id.etMessage))
+                .perform(typeText("hello"), closeSoftKeyboard());
+
+        onView(withId(R.id.btnSendChat)).perform(click());
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void verifyCall() {
+
+        ActivityScenario.launch(LoginActivity.class);
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(uName), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(pwd), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(client), closeSoftKeyboard());
+
+        // Clicks a button to send the message to another
+        // activity through an explicit intent.
+        onView(withId(R.id.button_login)).perform(click());
+
+        // Verifies that the DisplayMessageActivity received an intent
+        // with the correct package name and message.
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.container)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_call));
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.participant_address))
+                .perform(clearText(), typeText(participant_address), closeSoftKeyboard());
+
+        onView(withId(R.id.start_call_button)).perform(click());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.activeCallHangupButton)).perform(click());
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void verifyAddressbook() {
+
+        ActivityScenario.launch(LoginActivity.class);
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(uName), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(pwd), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(client), closeSoftKeyboard());
+
+        // Clicks a button to send the message to another
+        // activity through an explicit intent.
+        onView(withId(R.id.button_login)).perform(click());
+
+        // Verifies that the DisplayMessageActivity received an intent
+        // with the correct package name and message.
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.container)).check(matches(isDisplayed()));
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
+
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_addressbook));
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.recycleView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.et_last_name))
+                .perform(typeText("abcd"), closeSoftKeyboard());
+        onView(withId(R.id.button_create_contact)).perform(click());
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(isRoot()).perform(ViewActions.pressBack());
+    }
+
+    @Test
+    public void verifyGroupChat() {
+
+        ActivityScenario.launch(LoginActivity.class);
+        // Types a message into a EditText element.
+        onView(withId(R.id.et_url))
+                .perform(typeText(url), closeSoftKeyboard());
+        onView(withId(R.id.et_user_name))
+                .perform(typeText(uName), closeSoftKeyboard());
+        onView(withId(R.id.et_user_password))
+                .perform(typeText(pwd), closeSoftKeyboard());
+        onView(withId(R.id.et_user_client))
+                .perform(typeText(client), closeSoftKeyboard());
+
+        // Clicks a button to send the message to another
+        // activity through an explicit intent.
+        onView(withId(R.id.button_login)).perform(click());
+
+        // Verifies that the DisplayMessageActivity received an intent
+        // with the correct package name and message.
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.container)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_group_chat));
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // onView(withId(R.id.groupList))
+        //       .perform(ListVie.actionOnItemAtPosition(0, click()));
+        onData(anything()).inAdapterView(withId(R.id.groupList)).atPosition(0).perform(click());
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.messageEditText))
+                .perform(typeText("abcd"), closeSoftKeyboard());
+        onView(withId(R.id.sendMessage)).perform(click());
+    }
+/*
     @Test
     public void verifyMessageSentToMessageActivity() {
 
@@ -85,6 +406,8 @@ public class ExampleInstrumentedTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open()); // Open Drawer
 
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_sms));
@@ -142,7 +465,7 @@ public class ExampleInstrumentedTest {
             e.printStackTrace();
         }
         onView(withId(R.id.participant_address))
-                .perform(clearText(),typeText(participant_address), closeSoftKeyboard());
+                .perform(clearText(), typeText(participant_address), closeSoftKeyboard());
 
         onView(withId(R.id.start_call_button)).perform(click());
         try {
@@ -190,8 +513,8 @@ public class ExampleInstrumentedTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-       // onView(withId(R.id.groupList))
-         //       .perform(ListVie.actionOnItemAtPosition(0, click()));
+        // onView(withId(R.id.groupList))
+        //       .perform(ListVie.actionOnItemAtPosition(0, click()));
         onData(anything()).inAdapterView(withId(R.id.groupList)).atPosition(0).perform(click());
         try {
             Thread.sleep(20000);
@@ -202,5 +525,5 @@ public class ExampleInstrumentedTest {
                 .perform(typeText("abcd"), closeSoftKeyboard());
         onView(withId(R.id.sendMessage)).perform(click());
     }
-
+*/
 }
